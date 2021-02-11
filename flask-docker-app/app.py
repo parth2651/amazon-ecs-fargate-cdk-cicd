bc69062432134b1fc06a15b5bb571191fbac859c
EC2 Default User <ec2-user@ip-172-31-1-96.us-east-2.compute.internal>
2021-02-11 23:41:29 +0000

    adding new flask files

--- flask-docker-app/app.py
+++ flask-docker-app/app.py
@@ -1,12 +1,14 @@
 # from flask import Flask, escape, request, render_template
-import flask
+import flask, render_template, request
 import datetime
 import platform
 import os
+from application import db
+from application.models import Data
+from application.forms import EnterDBInfo, RetrieveDBInfo
+
 
 app = flask.Flask(__name__)
-
-
 @app.route('/')
 def hello():
     name = flask.request.args.get("name", "Flask-demo")
@@ -21,6 +23,32 @@ def hello():
                                  time=time,
                                  name=name)
 
+def index():
+    form1 = EnterDBInfo(request.form) 
+    form2 = RetrieveDBInfo(request.form) 
+    
+    if request.method == 'POST' and form1.validate():
+        data_entered = Data(notes=form1.dbNotes.data)
+        try:     
+            db.session.add(data_entered)
+            db.session.commit()        
+            db.session.close()
+        except:
+            db.session.rollback()
+        return render_template('thanks.html', notes=form1.dbNotes.data)
+        
+    if request.method == 'POST' and form2.validate():
+        try:   
+            num_return = int(form2.numRetrieve.data)
+            query_db = Data.query.order_by(Data.id.desc()).limit(num_return)
+            for q in query_db:
+                print(q.notes)
+            db.session.close()
+        except:
+            db.session.rollback()
+        return render_template('results.html', results=query_db, num_return=num_return)                
+    
+    return render_template('index.html', form1=form1, form2=form2)
 
 if __name__ == '__main__':
     app.run(
